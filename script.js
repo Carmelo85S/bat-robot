699511b942d6169a1a0ff7d30418d871fa574af0
Carmelo85S <carmelo.salis85@gmail.com>
2024-02-15 10:46:24 +0100

    good

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const isRobotOn = switchButton.classList.contains('on');
             if (!isRobotOn) {
                 switchButton.classList.add('on');
-                alert("Hi, I am Bat-Robot!"); // Visualizza un messaggio di avviso
+                alert("Hi, I am Bat-Robot!"); 
             } else {
                 switchButton.classList.remove('on');
             }
@@ -19,28 +19,25 @@ document.addEventListener('DOMContentLoaded', function () {
         smile.classList.toggle('hovered');
     });
 
-// Funzione per chiudere gli occhi
+
 function closeEyes() {
     document.querySelector('.right-eye').classList.add('closed');
     document.querySelector('.left-eye').classList.add('closed');
 }
 
-// Funzione per aprire gli occhi
 function openEyes() {
     document.querySelector('.right-eye').classList.remove('closed');
     document.querySelector('.left-eye').classList.remove('closed');
 }
-    // Cambia lo stato degli occhi ogni 4 secondi
+
     setInterval(function () {
         closeEyes();
-        setTimeout(openEyes, 200); // Apri gli occhi dopo 1 secondo di chiusura
-        }, 10000); // Esegui ogni 4 secondi
+        setTimeout(openEyes, 200);
+        }, 10000); 
     });
 
 function changeColor() {
-    // Genera un colore casuale in formato esadecimale
     var changeColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 
-    // Imposta il colore dello sfondo del corpo del documento
     document.body.style.backgroundColor = changeColor;
 }
