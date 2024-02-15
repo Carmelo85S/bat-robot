080212797612244f818b7a2d1554dc442ac024b8
Carmelo85S <carmelo.salis85@gmail.com>
2024-02-15 09:52:23 +0100

    adding bat robot project

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const centerCircle = document.getElementById('center-circle');
+    const leftEye = document.getElementById('leftEye');
+    const rightEye = document.getElementById('rightEye');
+    const smile = document.getElementById('smile');
+    const switchButton = document.getElementById('button');
+
+    switchButton.addEventListener('click', function () {
+        const isRobotOn = switchButton.classList.contains('on');
+            if (!isRobotOn) {
+                switchButton.classList.add('on');
+                alert("Hi, this is Bat-Robot!"); // Visualizza un messaggio di avviso
+            } else {
+                switchButton.classList.remove('on');
+            }
+        centerCircle.classList.toggle('hovered');
+        leftEye.classList.toggle('hovered');
+        rightEye.classList.toggle('hovered');
+        smile.classList.toggle('hovered');
+    });
+
+// Funzione per chiudere gli occhi
+function closeEyes() {
+    document.querySelector('.right-eye').classList.add('closed');
+    document.querySelector('.left-eye').classList.add('closed');
+}
+
+// Funzione per aprire gli occhi
+function openEyes() {
+    document.querySelector('.right-eye').classList.remove('closed');
+    document.querySelector('.left-eye').classList.remove('closed');
+}
+    // Cambia lo stato degli occhi ogni 4 secondi
+    setInterval(function () {
+        closeEyes();
+        setTimeout(openEyes, 200); // Apri gli occhi dopo 1 secondo di chiusura
+        }, 10000); // Esegui ogni 4 secondi
+    });
+
+function changeColor() {
+    // Genera un colore casuale in formato esadecimale
+    var changeColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+
+    // Imposta il colore dello sfondo del corpo del documento
+    document.body.style.backgroundColor = changeColor;
+}
